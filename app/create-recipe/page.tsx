aaa5f692ab185d8a09bc565ff7c0c8bb53757163
ItIsLukenTime <lmuhnick@stevens.edu>
2024-04-23 12:42:57 -0400

    revised create-recipe page

--- app/create-recipe/page.tsx
+++ app/create-recipe/page.tsx
@@ -11,21 +11,24 @@ export default function CreateRecipe() {
 
 	return (
 		<div>
-			<h1>Create a recipe here!</h1>
-			<p className="font-serif text-4xl">
-				Below, please type the directions for your recipe. When you are
+			<h1 className="font-sans font-semibold text-center text-3xl transform hover:scale-110">Create Recipe Page!</h1>
+			<p className="font-sans font-medium text-center text-lg">
+				Below, please type the title and instructions for your recipe. When you are
 				finished, press the blue button to submit.
 			</p>
+			<br></br>
 			<form>
-				<label id="recipeTitle">Recipe Title</label>
-				<br />
-				<Input type="text" id="recipeTitle" name="title" />
-				<br />
-				<label id="recipeDescription">Recipe Description</label>
-				<Input type="text" id="recipeDescription" name="description" />
-				<Button type="submit" value="Submit">
-					Submit here
-				</Button>
+				<div className="flex justify-center items-center mb-4">
+    				<label htmlFor="recipeTitle" className="font-sans font-normal mr-2">Title</label>
+    				<Input type="text" id="recipeTitle" name="title" className="w-full md:w-1/4" />
+  				</div>
+				<div className="flex justify-center items-center mb-4">
+					<label id="recipeInstructions" className="font-sans font-normal mr-2">Instructions</label>
+					<textarea id="recipeInstructions" name="instructions" className="w-full md:w-3/4 border-gray-300 rounded-md"/>
+				</div>
+				<div className="flex justify-center">
+					<Button type="submit" value="Submit" className="center">Submit here</Button>
+				</div>
 			</form>
 		</div>
 	);
