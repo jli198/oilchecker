0303daf0bffee5043a1db11819eb4111293a72e0
Rocco <rocco@vaccone.us>
2024-04-24 18:21:30 -0400

    Removed search input

--- app/HomeClientPage.tsx
+++ app/HomeClientPage.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
-import { createClientSupabase } from "@/utils/supabase/client";
-import { useState } from "react";
 import OilCard from "./OilCard";
 
 interface Oil {
@@ -14,13 +11,9 @@ interface Oil {
 }
 
 export default function HomeClientPage({ oilData }: { oilData: any }) {
-	const supabase = createClientSupabase();
-	const [oilSearchTerm, setOilSearchTerm] = useState<string>("");
-
 	return (
 		<div className="flex w-full flex-col items-center text-black">
-			<Input placeholder="Search for oils" className="my-10 w-3/5" />
-			<div className="mx-10 mb-10 flex flex-wrap justify-between space-y-5">
+			<div className="m-10 mb-10 flex flex-wrap justify-between space-y-5">
 				{oilData.map((oil: Oil) => (
 					<OilCard key={oil.id} {...oil} />
 				))}
