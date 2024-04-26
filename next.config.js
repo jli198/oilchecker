e34ce44e5da308bee7e2d19f3084849dd08f226e
Rocco <rocco@vaccone.us>
2024-04-25 22:42:23 -0400

    Updated config
    Co-Authored-By: Jason Li <84582864+jli198@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	env: {
+		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+		NEXT_PUBLIC_SUPABASE_ANON_KEY:
+			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
